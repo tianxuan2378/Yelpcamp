f29bae4a856ef2c53fa73ef5149f9c32c44a9eb2
tianxuan2378 <84616683+tianxuan2378@users.noreply.github.com>
2023-09-03 20:21:33 -0400

    Added map

--- seeds/index.js
+++ seeds/index.js
@@ -26,6 +26,10 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/dousp1zfy/image/upload/v1693689389/YelpCamp/duhlynlrgzx4xqvy4yws.jpg',
